fe29d58dd74ef673acf940f07e3e99950e4a0998
nissanursabrina <nissabrina12345@gmail.com>
2021-10-09 11:04:15 +0700

    add username input

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,9 +16,10 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Administrator',
-            'email' => 'admin@admin.com',
-            'password' => Hash::make('password'),
+            'username' => 'admin',
+            'name' => 'Administrator Baru',
+            'email' => 'adminbaru@gmail.com',
+            'password' => Hash::make('12345'),
         ]);
     }
 }
