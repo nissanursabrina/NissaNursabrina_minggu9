a531f3d185f40f1eda28549f04dbfcf1200078e1
nissanursabrina <nissabrina12345@gmail.com>
2021-10-17 18:24:23 +0700

    menampilkan data students

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('students',StudentController::class);
+Route::resource('students', StudentController::class);
