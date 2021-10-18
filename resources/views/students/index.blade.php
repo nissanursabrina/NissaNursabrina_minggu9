bc35b8abfa290e474d6bbc6fd0965b0875cc56b2
nissanursabrina <nissabrina12345@gmail.com>
2021-10-18 13:09:52 +0700

    tambah tombol view untuk menampilkan semua kolom dan valuenya dari data students

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -39,6 +39,7 @@
                                 <td>
                                 <form action="/students/{{$s->id}}" method="post">
                                     <a href="/students/{{$s->id}}/edit" class="btn btn-warning">Edit</a>
+                                    <a href="/students/{{$s->id}}" class="btn btn-info">View</a>
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" name="delete" class="btn btn-danger">Delete</button>
