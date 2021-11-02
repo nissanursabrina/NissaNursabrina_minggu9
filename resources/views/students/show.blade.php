1d62f09adad14ec1ca8874a9791690355aa6adde
nissanursabrina <nissabrina12345@gmail.com>
2021-11-02 21:35:58 +0700

    Ubah Menampilkan Detail Data

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -15,13 +15,23 @@
                     @endif
 
 
-                    <table class="table table-responsive">
+                    <!-- <table class="table table-responsive">
                         <tr><th>NIM</th><th>:</th><td>{{ $student->nim }}</td></tr>
                         <tr><th>Name</th><th>:</th><td>{{ $student->name }}</td></tr>
                         <tr><th>Class</th><th>:</th><td>{{ $student->class }}</td></tr>
                         <tr><th>Department</th><th>:</th><td>{{ $student->department}}</td></tr>
                         <tr><th>Phone Number</th><th>:</th><td>{{ $student->phone_number}}</td></tr>
-                    </table>
+                    </table> -->
+                    <div class="card-header">{{ $student->nim }}</div>
+                        <div class="card-body">
+                            Name : {{ $student->name }} <br>
+                            Class : {{ $student->kelas->class_name }} <br>
+                            Department : {{ $student->department }} <br>
+                            Phone Number : {{ $student->phone_number }} <br>
+                            Created at : {{ $student->created_at }} <br>
+                            Updated at : {{ $student->updated_at }} <br>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
